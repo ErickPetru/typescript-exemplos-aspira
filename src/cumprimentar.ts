f7939d83d5f30fcc6e65f7cf8654bb5e0594e299
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-10-28 14:16:45 -0300

    Passa os códigos das telas para escopo por bloco

--- src/cumprimentar.ts
+++ src/cumprimentar.ts
@@ -1,20 +1,22 @@
-const campo = document.querySelector<HTMLInputElement>('.custom-input')!
-const formulario = document.querySelector<HTMLFormElement>('form')!
+{
+  const campo = document.querySelector<HTMLInputElement>('.custom-input')!
+  const formulario = document.querySelector<HTMLFormElement>('form')!
 
-formulario.addEventListener('submit', (event: Event) => {
-  event.preventDefault()
+  formulario.addEventListener('submit', (event: Event) => {
+    event.preventDefault()
 
-  let p = document.querySelector<HTMLParagraphElement>('#mensagem')
+    let p = document.querySelector<HTMLParagraphElement>('#mensagem')
 
-  if (!p) {
-    p = document.createElement('p')
-    p.id = 'mensagem'
-    formulario.append(p)
-  }
+    if (!p) {
+      p = document.createElement('p')
+      p.id = 'mensagem'
+      formulario.append(p)
+    }
 
-  if (campo.value) {
-    p.innerText = `Olá, ${campo.value}!`
-  } else {
-    p.innerText = 'Olá mundo!'
-  }
-})
+    if (campo.value) {
+      p.innerText = `Olá, ${campo.value}!`
+    } else {
+      p.innerText = 'Olá mundo!'
+    }
+  })
+}
